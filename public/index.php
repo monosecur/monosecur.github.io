76f2cff29d7e43bcdebe72432a423121e956b694
DarkSideMonolith <112771274+flush286@users.noreply.github.com>
2022-09-22 19:18:22 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -16,11 +16,19 @@
     <p>Email : <?= $email; ?></p>
     <p>Mot de Passe : <?= $password; ?></p>
 
-    <form>
+    <form method="post">
         <input type="text" name="Pseudo" id="pseudo" placeholder="Pseudonyme">
         <input type="text" name="Email" id="email" placeholder="Email">
         <input type="text" name="Mot de Passe" id="password" placeholder="Mot de Passe">
         <input type="submit" name="Envoie du formulaire" id="formsend">
     </form>
+
+    <?php
+    
+        if(isset($_POST['formsend'])){
+            echo "Formulaire envoyer !";
+        }
+    
+    ?>
 </body>
 </html>
