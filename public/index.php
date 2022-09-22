20b9066e47f83e2e1f5ef165bed54cc4a146f320
DarkSideMonolith <112771274+flush286@users.noreply.github.com>
2022-09-22 18:26:08 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,19 @@
+<?php
+
+    $pseudo = "Flush";
+    $email = "flush@mail.com";
+    $password = "theflusher286";
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Titre</title>
 </head>
 <body>
-    <p>Nom : </p>
-    <p>Email : </p>
-    <p>Mot de Passe : </p>
+    <p>Nom : <?= $pseudo; ?></p>
+    <p>Email : <?= $email; ?></p>
+    <p>Mot de Passe : <?= $password; ?></p>
 </body>
 </html>
