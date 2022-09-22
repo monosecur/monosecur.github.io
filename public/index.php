3d885f68e3c5992a5f16067799b6d04c62dc9501
DarkSideMonolith <112771274+flush286@users.noreply.github.com>
2022-09-22 18:17:32 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,11 @@
-<?php echo "string"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Titre</title>
+</head>
+<body>
+    <p>Nom : </p>
+    <p>Email : </p>
+    <p>Mot de Passe : </p>
+</body>
+</html>
