434bd0eb13a82d7d93806d04df12fea3d11860b6
DarkSideMonolith <112771274+flush286@users.noreply.github.com>
2022-09-22 18:55:54 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -15,5 +15,12 @@
     <p>Nom : <?= $pseudo; ?></p>
     <p>Email : <?= $email; ?></p>
     <p>Mot de Passe : <?= $password; ?></p>
+
+    <form>
+        <input type="text" name="Pseudo" id="pseudo">
+        <input type="text" name="Email" id="email">
+        <input type="text" name="Mot de Passe" id="password">
+        <input type="submit" name="Envoie du formulaire" id="formsend">
+    </form>
 </body>
 </html>
