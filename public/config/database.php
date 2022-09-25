f0aba6e72e1a36a03c439154793c66629a8c6737
flush286 <112771274+flush286@users.noreply.github.com>
2022-09-25 11:37:20 +0200

    commit

--- public/config/database.php
+++ public/config/database.php
@@ -8,7 +8,7 @@
     try{
         $db = new PDO("mysql:host=" . HOST . ";dbname=" .DB_NAME, USER, PASS);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connect > OK !";
+        echo "Connected succesfully to DB <br>";
     } catch(PDOException $e){
         echo $e;
     }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
+<?php session_start(); 
+
+    $_SESSION['pseudo'] = "MonPseudo";
+
+    var_dump($_SESSION);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,27 +12,64 @@
 </head>
 <body>
 
+    <h1>Bienvenu sur votre profile</h1>
+    <p>votre Pseudo : <?= $_SESSION['pseudo'];?></p>
+
     <form method="post">
-        <input type="text" name="pseudo" id="pseudo" placeholder="Pseudonyme" required><br/>
-        <input type="text" name="email" id="email" placeholder="Email" required><br/>
-        <input type="text" name="password" id="password" placeholder="Mot de Passe" required><br/>
-        <input type="submit" name="formsend" id="formsend">
+    <input type="email" name="email" id="email" placeholder="Email" required><br/>
+    <input type="password" name="password" id="password" placeholder="Mot de Passe" required><br/>
+    <input type="password" name="cpassword" id="cpassword" placeholder="Confirmer Mot de Passe" required><br/>
+    <input type="submit" name="formsend" id="formsend">
     </form>
 
     <?php
     
-        include'config/database.php';
-    
+
         if(isset($_POST['formsend'])){
+
+            extract($_POST);
+
             
-            if(!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password'])){
 
-                echo "votre pseudo est ".$_POST['pseudo']." et votre email est ".$_POST['email'];                
+            if(!empty($password) && !empty($cpassword) && !empty($email)){
+                
 
-            }
-
-        }
+                if($password == $cpassword){
+                    $options = [
+                        'cost' => 12,
+                    ];
     
+                    $hashpass = password_hash($password, PASSWORD_BCRYPT, $options);
+
+                    include'config/database.php';
+                    global $db;
+
+                    $c = $db->prepare("SELECT email FROM users WHERE email = :email");
+                    $c->execute(['email' => $email]);
+
+                    $result = $c->rowCount();
+                    
+
+                    if($result == 0){
+                        $q = $db->prepare("INSERT INTO users(email,password) VALUES(:email,:password)");
+                    $q->execute([
+                        'email' => $email,
+                        'password' => $hashpass
+                    ]);
+                    echo "Formulaire rempli avec succes";
+                    }else{
+                        echo "un email existe deja !";
+                    }
+
+                }else{
+                    echo "Les deux mot de passes ne correspondent pas !";
+                }
+
+            }else{
+                echo "Les champs ne sont pas tous remplies";
+            }
+        }
+
     ?>
 </body>
 </html>
