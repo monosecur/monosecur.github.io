501ae8a9635c9bfa162ebd28c3c8485da35fe894
flush286 <112771274+flush286@users.noreply.github.com>
2022-10-16 14:38:18 +0200

    e

---
+++ public/config/phpmailer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '12afb48d5283f8b281f24d11a53f4de66d17fef5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '12afb48d5283f8b281f24d11a53f4de66d17fef5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.4',
+            'version' => '6.6.4.0',
+            'reference' => 'a94fdebaea6bd17f51be0c2373ab80d3d681269b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
--- public/config/register.php
+++ public/config/register.php
@@ -1,6 +1,11 @@
 <?php
 
-
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\SMTP;
+//use PHPMailer\PHPMailer\Exception;
+//require 'phpmailer/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+//require'phpmailer/vendor/phpmailer/phpmailer/src/SMTP.php';
+//require'phpmailer/vendor/phpmailer/phpmailer/src/Exception.php';
     
 
     if(isset($_POST['formsend'])){
@@ -45,7 +50,62 @@
                         'email' => $email,
                         'password' => $hashpass
                         ]);
-                        echo "Vous pouvez désormais vous connecter.";
+                        
+                        include 'hello.php';
+                        //require 'phpmailer/vendor/autoload.php';
+
+                        //$mail = new PHPMailer(true);
+
+                       // try {
+                            //Server settings
+                           // $mail->SMTPDebug = 2;                      //Enable verbose debug output
+                           // $mail->isSMTP();                                            //Send using SMTP
+                           // $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                           // $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                           // $mail->Username   = 'darkside.monolith@gmail.com';                     //SMTP username
+                            //$mail->Password   = 'kvtjguknaeaptcks';                               //SMTP password
+                          //  $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+                         //   $mail->Port       = 587;///465                                 //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                            //Recipients
+                           // $mail->setFrom('darkside.monolith@gmail.com', 'Mono Secur');
+                          //  $mail->addAddress($email);     //Add a recipient
+
+
+                            //Content
+                           // $mail->isHTML(true);                                  //Set email format to HTML
+                           // $mail->Subject = 'Here is the subject';
+                           // $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                           // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                           // $mail->send();
+                          //  echo 'Message has been sent';
+                     //   } catch (Exception $e) {
+                     //       echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                     //   }
+
+                        //$mheader="MIME-VERSION: 1.0\r\n";
+                        //$mheader.='From: Mono Secur <darkside.monolith@gmail.com>'."\n";
+                        //$mheader.='Content-Type:text/html; charset="uft-8"'."\n";
+                        //$mheader.='Content-Transfer-Encoding: 8bit';
+
+                        //$mmessage='
+                        
+                            //<html>
+                                //<body>
+                                    //<div align="center">
+                                        //<a href="https://monosecur.epizy.com/public/emailverify.php?token='.urlencode($token).'">Confirmez votre adresse mail !</a>
+                                    //</div>
+                                //</body>
+                            //</html>
+                        
+                        //';
+
+                        if (mail($email, "Comfirmer votre addresse mail", $mmessage, $mheader))
+                            echo "Nous avons envoyer un mail à ".$email.", merci de confirmer votre email pour vous connecter !";
+                        else
+                            echo "stuff went wrong";
+                        
                     }else{
                         echo "Ce pseudo est déjà utiliser !";
                     }
