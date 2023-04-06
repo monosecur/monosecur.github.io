de3e8507b7c355f54617fd28f4ccc43c22a87a7b
ZlaWox <130023842+ZlaWox@users.noreply.github.com>
2023-04-06 07:31:30 +0300

    Update login_menu.php
    improved formatting

--- login_menu.php
+++ login_menu.php
@@ -6,11 +6,10 @@
   <title class="indextitle">Mono Secur</title>
   <link rel="stylesheet" href="CSStyle/login_menu.css">
   <link rel="preconnect" href="https://fonts.googleapis.com"> 
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;500;700&display=swap" rel="stylesheet">
-<script src="https://kit.fontawesome.com/a7819a9eea.js" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;500;700&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/a7819a9eea.js" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 <body>
   <section class="header">
@@ -26,29 +25,23 @@
       </div>
       <i class="fa-sharp fa-bars" onclick="ShowMenu()"></i>
     </nav>
-
-<div class="text-box">
-
-  <h1>Mono Secur</h1>
-  <p class="index-text">Ce site est entierement une fiction créer pour un serveur d'un jeux vidéo,<br>pour plus d'information cliquez sur le lien ci-dessous :</p>
-  <p class="phone-text">Sorry but this website don't work on you'r web browser or device.<br>désolé mais ce site ne fonctionne pas sur votre navigateur internet ou votre appareil.</p>
-  <a href="https://monolithservers.fr" target="_blank" class="hero-btn">Accéder au site officiel de Monolith.</a>
-</div>
-
+    <div class="text-box">
+      <h1>Mono Secur</h1>
+      <p class="index-text">Ce site est entierement une fiction créer pour un serveur d'un jeux vidéo,<br>pour plus d'information cliquez sur le lien ci-dessous :</p>
+      <p class="phone-text">Sorry but this website don't work on you'r web browser or device.<br>désolé mais ce site ne fonctionne pas sur votre navigateur internet ou votre appareil.</p>
+      <a href="https://monolithservers.fr" target="_blank" class="hero-btn">Accéder au site officiel de Monolith.</a>
+    </div>
   </section>
 
-
-<!----Javascript for menu buttons---->
-<script>
+  <!-- JavaScript for menu buttons -->
+  <script>
     var navLinks = document.getElementById("navLinks")
-  function ShowMenu(){
-    navLinks.style.right = "0";
-  }
-  function HideMenu(){
-    navLinks.style.right = "-200px";
-  }
-</script>
-
-
+    function ShowMenu(){
+      navLinks.style.right = "0";
+    }
+    function HideMenu(){
+      navLinks.style.right = "-200px";
+    }
+  </script>
 </body>
 </html>
