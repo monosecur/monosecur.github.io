6f4cad4f7ed7a7ee7c35c6a41fc6758e714ddb35
ZlaWox <130023842+ZlaWox@users.noreply.github.com>
2023-04-06 07:27:01 +0300

    Update contextmenu.php
    I removed the external JavaScript and CSS sources, and replaced them with inline styles and a placeholder function for the onclick, still need to modify the function names and styles

--- staff/contextmenu.php
+++ staff/contextmenu.php
@@ -1,32 +1,45 @@
-<html>
-  <head>
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9734489296512588" crossorigin="anonymous"></script>
-  <title>Mono Secur</title>
-  <script src="https://kit.fontawesome.com/a7819a9eea.js" crossorigin="anonymous"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-  <script src="https://monosecur.tk/JScript/contextmenu-script.js"></script>
-  <link rel="stylesheet" href="http://monosecur.tk/CSStyle/staff/utilisateurs.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com"> 
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;500;700&display=swap" rel="stylesheet">
-  </head>
-  <body>
- <div id="contextmenu">
-  <div class="list">
-    <div class="list">
-      <i class="fa-solid fa-pen-to-square">
-        <button onclick="myFunction()" id="modify">
-        Modifier
-      </button>
-      </i>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mono Secur</title>
+    <link rel="stylesheet" href="http://monosecur.tk/CSStyle/staff/utilisateurs.css">
+    <style>
+        /* CSS styles for contextmenu */
+        #contextmenu {
+            /* Add your styles here */
+        }
+        .list {
+            /* Add your styles here */
+        }
+        #modify, #default {
+            /* Add your styles here */
+        }
+    </style>
+</head>
+<body>
+    <div id="contextmenu">
+        <div class="list">
+            <div class="list">
+                <i class="fa-solid fa-pen-to-square">
+                    <button onclick="modifyFunction()" id="modify">
+                        Modifier
+                    </button>
+                </i>
+            </div>
+            <i class="fa-solid fa-trash">
+                <button id="default">
+                    défault
+                </button>
+            </i>
+        </div>
     </div>
-      <i class="fa-solid fa-trash">
-      <button id="default">
-        défault
-      </button>
-      </i>
-    </div>
-  </div>
-</div>
-  </body>
+
+    <!-- JavaScript code for contextmenu -->
+    <script>
+        function modifyFunction() {
+            // Add your function code here
+        }
+    </script>
+</body>
 </html>
