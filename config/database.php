cf36e9eca057590fb5136fc9cd6933a3779577bd
flush286 <112771274+flush286@users.noreply.github.com>
2023-07-24 01:36:18 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,8 +3,8 @@
     if (!defined('HOST')) define('HOST', 'localhost');
     if(!defined('PORT')) define('PORT', '3306');
     if (!defined('DB_NAME')) define('DB_NAME','monosecur_db');
-    if (!defined('USER')) define('USER','bob31');
-    if (!defined('PASS')) define('PASS','w_3Fj18m4');
+    if (!defined('USER')) define('USER','None');
+    if (!defined('PASS')) define('PASS','None');
 
     try{
         $db = new PDO("mysql:host=" . HOST . ";port=" . PORT . ";dbname=" .DB_NAME, USER, PASS);
